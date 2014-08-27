c4b2e73523c003e26661b1c0d6fed049cd1a8381
EWB <ewil@stlawu.edu>
2014-08-27 12:12:59 -0400

    addion of digital theme and beforeafter module and library

---
+++ sites/all/themes/digitalradix/template.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @file
+ * Theme functions
+ */
+
+require_once dirname(__FILE__) . '/includes/structure.inc';
+require_once dirname(__FILE__) . '/includes/comment.inc';
+require_once dirname(__FILE__) . '/includes/form.inc';
+require_once dirname(__FILE__) . '/includes/menu.inc';
+require_once dirname(__FILE__) . '/includes/node.inc';
+require_once dirname(__FILE__) . '/includes/panel.inc';
+require_once dirname(__FILE__) . '/includes/user.inc';
+require_once dirname(__FILE__) . '/includes/view.inc';
+
+/**
+ * Implements hook_css_alter().
+ */
+function digitalradix_css_alter(&$css) {
+  $radix_path = drupal_get_path('theme', 'radix');
+
+  // Radix now includes compiled stylesheets for demo purposes.
+  // We remove these from our subtheme since they are already included 
+  // in compass_radix.
+  unset($css[$radix_path . '/assets/stylesheets/radix-style.css']);
+  unset($css[$radix_path . '/assets/stylesheets/radix-print.css']);
+}
+
+/**
+ * Implements template_preprocess_page().
+ */
+function digitalradix_preprocess_page(&$variables) {
+  // Add copyright to theme.
+  if ($copyright = theme_get_setting('copyright')) {
+    $variables['copyright'] = check_markup($copyright['value'], $copyright['format']);
+  }
+
+  // Add search_form to theme.
+  $variables['search_form'] = '';
+  if (module_exists('search') && user_access('search content')) {
+    //jfl $search_box_form = drupal_get_form('search_form');
+    $search_box_form = drupal_get_form('search_block_form');
+    $search_box_form['basic']['keys']['#title'] = '';
+    $search_box_form['basic']['keys']['#size'] = 20;
+    $search_box_form['basic']['keys']['#attributes'] = array('placeholder' => 'Search');
+    $search_box_form['basic']['keys']['#attributes']['class'][] = 'form-control';
+    $search_box_form['basic']['submit']['#value'] = t('Search');
+    $search_box_form['#attributes']['class'][] = 'navbar-form';
+    $search_box_form['#attributes']['class'][] = 'navbar-right';
+    $search_box = drupal_render($search_box_form);
+    $variables['search_form'] = (user_access('search content')) ? $search_box : NULL;
+  }
+}
